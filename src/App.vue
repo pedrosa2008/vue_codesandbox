79af00a669530cfcb2c0b669d20654b21781f0b1
Fernando Pedrosa <pedrosa2008@gmail.com>
2018-07-11 14:16:09 -0300

    Alteração no Layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <section id="app">
     <section class="header">
-      <section class="header-logo">
-        <img src="./assets/logo.png">
-      </section>
       <!-- Main container -->
       <nav class="level">
         <!-- Left side -->
         <div class="level-left">
           <div class="level-item">
+            <section class="header-logo">
+              <figure class="image is-48x48">
+                <img src="https://br.vuejs.org/images/logo.png">
+              </figure>
+            </section>
             <p class="title">Vuejs App</p>
           </div>
         </div>
 
         <!-- Right side -->
         <div class="level-right">
-          <p class="level-item"><strong>All</strong></p>
-          <p class="level-item"><a>Published</a></p>
-          <p class="level-item"><a>Drafts</a></p>
-          <p class="level-item"><a>Deleted</a></p>
+          <p class="level-item"><strong>Dashboard</strong></p>
+          <p class="level-item"><a>Usuários</a></p>
+          <p class="level-item"><a>Configurações</a></p>
         </div>
       </nav>
     </section>
 
-    <section class="section">
-      <section class="container is-fluid">
-        <router-view />
-      </section>
+    <section class="column">
+      <router-view />
     </section>
 
     <footer class="footer">
@@ -54,15 +53,10 @@
     background: #f5f7f9;
     position: relative;
     overflow: hidden;
-    padding: 2rem 1.5rem 2rem;
+    padding: 1rem;
   }
-  .header-logo{
-    width: 100px;
-    color: #fff;
-    border-radius: 3px;
-    float: left;
-    position: relative;
-    left: 20px;
+  .header-logo {
+    width: 60px;
   }
   .footer {
     padding: 1.5rem;
--- src/components/FormBox.vue
+++ src/components/FormBox.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-      <b-message :title="title" :closable="false">
+  <div class="box">
+    <div class="media-content">
+      <div class="content">
         <b-tag type="is-warning" v-show="message">{{message}}</b-tag>
         <slot></slot>
-        <hr/>
-        <p class="buttons is-pulled-right">
-          <a class="button is-primary" v-on:click="$emit('save')" >Save</a>
-          <a class="button is-secondary" @click="onClearClick" >Clear</a>
-        </p>
-      </b-message>
+      </div>
+    </div>
+
+    <div class="media-footer">
+      <hr/>
+      <div class="buttons has-addons is-right">
+        <a class="button is-secondary" @click="onClearClick" >Clear</a>
+        <a class="button is-primary" v-on:click="$emit('save')" >Save</a>
+      </div>
+    </div>
   </div>
 </template>
 
