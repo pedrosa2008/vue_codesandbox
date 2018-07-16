1668c4c6019bcfacb66e43b017afb3efb695a5cc
Fernando Pedrosa <pedrosa2008@gmail.com>
2018-07-16 15:51:43 -0300

    Alterações

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -7,47 +7,56 @@
         <div class="tile">
           <div class="tile is-parent">
             <article class="tile is-child notification is-info">
-              <p class="title">One</p>
+              <p class="title">Entradas</p>
               <p class="subtitle">Subtitle</p>
             </article>
           </div>
           <div class="tile is-parent">
             <article class="tile is-child notification is-info">
-              <p class="title">One</p>
+              <p class="title">Saídas</p>
               <p class="subtitle">Subtitle</p>
             </article>
           </div>
           <div class="tile is-parent">
             <article class="tile is-child notification is-info">
-              <p class="title">One</p>
+              <p class="title">Saldo</p>
               <p class="subtitle">Subtitle</p>
             </article>
           </div>
           <div class="tile is-parent">
             <article class="tile is-child notification is-info">
-              <p class="title">One</p>
+              <p class="title">Cartão MasterCard</p>
               <p class="subtitle">Subtitle</p>
             </article>
           </div>
           <div class="tile is-parent">
             <article class="tile is-child notification is-info">
-              <p class="title">One</p>
+              <p class="title">Cartão Visa</p>
               <p class="subtitle">Subtitle</p>
             </article>
           </div>
         </div>
       </div>
     </div>
+    
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <article class="tile is-child box">
+          <p class="title">Datatable de Histórico</p>
+          <p class="subtitle">Subtitle</p>
+        </article>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-  export default {
-    name: "Dashboard",
-    data() {
-      return {
-        pageName: "Dashboard Page"
-      }
-    }
-  };
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      pageName: "Dashboard Page"
+    };
+  }
+};
 </script>
